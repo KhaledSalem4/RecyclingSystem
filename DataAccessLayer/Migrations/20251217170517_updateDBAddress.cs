0ddd4558d4e93fab3a119d13c0983251ee156d2c
KhaledSalem4 <khaledas418@gmail.com>
2025-12-17 19:11:09 +0200

    Add Address as From To "Collector" and Update-Database

---
+++ DataAccessLayer/Migrations/20251217170517_updateDBAddress.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateDBAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Apartment",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BuildingNo",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Street",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Area",
+                table: "Factories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BuildingNo",
+                table: "Factories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Factories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Street",
+                table: "Factories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Apartment",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "BuildingNo",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Street",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Area",
+                table: "Factories");
+
+            migrationBuilder.DropColumn(
+                name: "BuildingNo",
+                table: "Factories");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Factories");
+
+            migrationBuilder.DropColumn(
+                name: "Street",
+                table: "Factories");
+        }
+    }
+}
